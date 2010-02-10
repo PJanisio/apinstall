0e577e9df53d5605d5a1c7c7d019688125911e5f
p.janisio@gmail.com <p.janisio@gmail.com@654e3746-162d-11df-a67c-bfef0438d298>
2010-02-10 12:00:12 +0000

    Added progress bar

--- trunk/logprint.php
+++ trunk/logprint.php
@@ -1,7 +1,23 @@
 <?php
 
-$f = file('l.log');
+
+$steps = 4;
 $lines = count(file('l.log'));
+
+$width = ($lines/$steps)*100;
+
+
+
+
+echo'<div class="meter-wrap">
+    <div class="meter-value" style="background-color: #0a0; width: '.$width.'%;">
+        <div class="meter-text">
+            '.$lines.'/'.$steps.'
+        </div>
+    </div>
+</div><br>';
+
+$f = file('l.log');
 echo $f[$lines - 1];
 
 ?>
