3e44a513bf4253bf4478d1658faa1c5900a3b836
p.janisio <p.janisio@gmail.com>
2010-02-10 18:01:15 +0000

    Added __construct class which include jQuery if user will not block it, changed delay functions, you can use now float integers.

--- install.php
+++ install.php
@@ -1,48 +1,53 @@
 <html>
 <head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 </head>
 <title>APINSTALL</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link href="bar.css" rel="stylesheet" type="text/css" />
 <body>
-<script type="text/javascript">
-var refreshId = setInterval(function()
-{
-     $('#install').load('print.php');
-}, 500);
-</script>
+
 
 <?php
 
 require_once('class/class.install.php');
-$install = new Installer;
+$install = new Installer();
 
-global $install;
 
 $install->setLogPath('l.log');
-$install->setSteps(4);
-$install->generatePrint(4);
+$install->setSteps(6);
+$install->generatePrint();
 
 
 
 
-echo '<div id="install"></div>';
+echo '<div id="apinstall"></div>';
 
 
 $output = 'Starting nuclear reactions...';
 $install->save($output);
-$install->sleepThicks(1);
+$install->delay(1);
+
 $output = 'Clearing cached data';
 $install->save($output);
-$install->sleepThicks(2);
-$output = 'Finishing';
+$install->delay(2);
+
+$output = 'Polishing';
 $install->save($output);
-$install ->sleepThicks(4);
+$install ->delay(0.5);
+
+$output = 'Making blowjob';
+$install->save($output);
+$install ->delay(2);
+
+$output = 'Farting loudly';
+$install->save($output);
+$install ->delay(2);
+
 $output = 'Done';
 $install->save($output);
+$install ->delay(5);
 
-$install->clearFiles();
+$install->clearTemp();
 
 
 
